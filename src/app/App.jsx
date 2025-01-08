430082ea3e00714c81effe3183636fd4816c966e
Vasya <VasyaPain2004@gmail.com>
2025-01-08 17:33:44 +0900

    git init

---
+++ src/app/App.jsx
@@ -0,0 +1,36 @@
+import Users from "./components/Users";
+import SearchStatus from "./components/SearchStatus";
+import api from "./api";
+import { useState } from "react";
+
+function App() {
+  const [users, setUsers] = useState(api.users.fetchAll());
+
+  const handleDelete = (userId) => {
+    setUsers(users.filter((user) => user._id !== userId));
+  };
+
+  const handleToggleBookMark = (id) => {
+    setUsers(
+      users.map((user) => {
+        if (user._id === id) {
+          return { ...user, bookmark: !user.bookmark };
+        }
+        return user;
+      })
+    );
+
+  };
+  return (
+    <div>
+      <SearchStatus length={users.length} />
+      <Users
+        onDelete={handleDelete}
+        onToggleBookMark={handleToggleBookMark}
+        users={users}
+      />
+    </div>
+  );
+}
+
+export default App;
